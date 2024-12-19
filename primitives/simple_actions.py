c7dbdd3bfdae9713a3d4e2e0346c924c295acc6d
Maksik-ko <koopmaksim0906@gmail.com>
2024-12-19 12:02:10 +0300

    simple_act

--- primitives/simple_actions.py
+++ primitives/simple_actions.py
@@ -2,78 +2,131 @@ from craftax.craftax.constants import Action
 from .executor import executor
 
 
-def act_DO(env):
-    action = Action.DO
-    executor(env, [action])
-
-def act_PLACE_STONE(env):
-    action = Action.PLACE_STONE
-    executor(env, [action])
-
-def act_PLACE_TABLE(env):
-    action = Action.PLACE_TABLE
-    executor(env, [action])
-
-def act_PLACE_FURNACE(env):
-    action = Action.PLACE_FURNACE
-    executor(env, [action])
-
-def act_PLACE_PLANT(env):
-    action = Action.PLACE_PLANT
-    executor(env, [action])
-
-def act_MAKE_WOOD_PICKAXE(env):
-    action = Action.MAKE_WOOD_PICKAXE
-    executor(env, [action])
-
-def act_MAKE_STONE_PICKAXE(env):
-    action = Action.MAKE_STONE_PICKAXE
-    executor(env, [action])
-
-def act_MAKE_IRON_PICKAXE(env):
-    action = Action.MAKE_IRON_PICKAXE
-    executor(env, [action])
-
-def act_MAKE_WOOD_SWORD(env):
-    action = Action.MAKE_WOOD_SWORD
-    executor(env, [action])
-
-def act_MAKE_STONE_SWORD(env):
-    action = Action.MAKE_STONE_SWORD
-    executor(env, [action])
-
-def act_MAKE_IRON_SWORD(env):
-    action = Action.MAKE_IRON_SWORD
-    executor(env, [action])
-
-def act_MAKE_DIAMOND_PICKAXE(env):
-    action = Action.MAKE_DIAMOND_PICKAXE
-    executor(env, [action])
-
-def act_MAKE_DIAMOND_SWORD(env):
-    action = Action.MAKE_DIAMOND_SWORD
-    executor(env, [action])
-
-def act_MAKE_IRON_ARMOUR(env):
-    action = Action.MAKE_IRON_ARMOUR
-    executor(env, [action])
-
-def act_MAKE_DIAMOND_ARMOUR(env):
-    action = Action.MAKE_DIAMOND_ARMOUR
-    executor(env, [action])
-
-def act_UP(env):
-    action = Action.UP
-    executor(env, [action])
-
-def act_DOWN(env):
-    action = Action.DOWN
-    executor(env, [action])
+def act_NOOP(env):
+    executor(env, [Action.NOOP])
 
 def act_LEFT(env):
-    action = Action.LEFT
-    executor(env, [action])
+    executor(env, [Action.LEFT])
 
 def act_RIGHT(env):
-    action = Action.RIGHT
-    executor(env, [action])
+    executor(env, [Action.RIGHT])
+
+def act_UP(env):
+    executor(env, [Action.UP])
+
+def act_DOWN(env):
+    executor(env, [Action.DOWN])
+
+def act_DO(env):
+    executor(env, [Action.DO])
+
+def act_SLEEP(env):
+    executor(env, [Action.SLEEP])
+
+def act_PLACE_STONE(env):
+    executor(env, [Action.PLACE_STONE])
+
+def act_PLACE_TABLE(env):
+    executor(env, [Action.PLACE_TABLE])
+
+def act_PLACE_FURNACE(env):
+    executor(env, [Action.PLACE_FURNACE])
+
+def act_PLACE_PLANT(env):
+    executor(env, [Action.PLACE_PLANT])
+
+def act_MAKE_WOOD_PICKAXE(env):
+    executor(env, [Action.MAKE_WOOD_PICKAXE])
+
+def act_MAKE_STONE_PICKAXE(env):
+    executor(env, [Action.MAKE_STONE_PICKAXE])
+
+def act_MAKE_IRON_PICKAXE(env):
+    executor(env, [Action.MAKE_IRON_PICKAXE])
+
+def act_MAKE_WOOD_SWORD(env):
+    executor(env, [Action.MAKE_WOOD_SWORD])
+
+def act_MAKE_STONE_SWORD(env):
+    executor(env, [Action.MAKE_STONE_SWORD])
+
+def act_MAKE_IRON_SWORD(env):
+    executor(env, [Action.MAKE_IRON_SWORD])
+
+def act_REST(env):
+    executor(env, [Action.REST])
+
+def act_DESCEND(env):
+    executor(env, [Action.DESCEND])
+
+def act_ASCEND(env):
+    executor(env, [Action.ASCEND])
+
+def act_MAKE_DIAMOND_PICKAXE(env):
+    executor(env, [Action.MAKE_DIAMOND_PICKAXE])
+
+def act_MAKE_DIAMOND_SWORD(env):
+    executor(env, [Action.MAKE_DIAMOND_SWORD])
+
+def act_MAKE_IRON_ARMOUR(env):
+    executor(env, [Action.MAKE_IRON_ARMOUR])
+
+def act_MAKE_DIAMOND_ARMOUR(env):
+    executor(env, [Action.MAKE_DIAMOND_ARMOUR])
+
+def act_SHOOT_ARROW(env):
+    executor(env, [Action.SHOOT_ARROW])
+
+def act_MAKE_ARROW(env):
+    executor(env, [Action.MAKE_ARROW])
+
+def act_CAST_FIREBALL(env):
+    executor(env, [Action.CAST_FIREBALL])
+
+def act_CAST_ICEBALL(env):
+    executor(env, [Action.CAST_ICEBALL])
+
+def act_PLACE_TORCH(env):
+    executor(env, [Action.PLACE_TORCH])
+
+def act_DRINK_POTION_RED(env):
+    executor(env, [Action.DRINK_POTION_RED])
+
+def act_DRINK_POTION_GREEN(env):
+    executor(env, [Action.DRINK_POTION_GREEN])
+
+def act_DRINK_POTION_BLUE(env):
+    executor(env, [Action.DRINK_POTION_BLUE])
+
+def act_DRINK_POTION_PINK(env):
+    executor(env, [Action.DRINK_POTION_PINK])
+
+def act_DRINK_POTION_CYAN(env):
+    executor(env, [Action.DRINK_POTION_CYAN])
+
+def act_DRINK_POTION_YELLOW(env):
+    executor(env, [Action.DRINK_POTION_YELLOW])
+
+def act_READ_BOOK(env):
+    executor(env, [Action.READ_BOOK])
+
+def act_ENCHANT_SWORD(env):
+    executor(env, [Action.ENCHANT_SWORD])
+
+def act_ENCHANT_ARMOUR(env):
+    executor(env, [Action.ENCHANT_ARMOUR])
+
+def act_MAKE_TORCH(env):
+    executor(env, [Action.MAKE_TORCH])
+
+def act_LEVEL_UP_DEXTERITY(env):
+    executor(env, [Action.LEVEL_UP_DEXTERITY])
+
+def act_LEVEL_UP_STRENGTH(env):
+    executor(env, [Action.LEVEL_UP_STRENGTH])
+
+def act_LEVEL_UP_INTELLIGENCE(env):
+    executor(env, [Action.LEVEL_UP_INTELLIGENCE])
+
+def act_ENCHANT_BOW(env):
+    executor(env, [Action.ENCHANT_BOW])
